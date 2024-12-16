7393193777c79af98e8c841d151cebf1c07d3c16
Muzahid Ansari <mujahidansari434@gmail.com>
2024-12-16 14:52:46 +0530

    First commit

---
+++ Assingment1.js
@@ -0,0 +1,132 @@
+/* this document contains implementation of the following topic of java script :
+
+ JavaScript Variables
+ JavaScript Data Types
+ JavaScript Operators
+ JavaScript Functions
+ JavaScript Arrays
+ JavaScript Loops
+ JavaScript Conditionals
+
+ and the remaining topics i am learning, after learning i will push them on git hub
+ one by one
+*/
+
+// uncoment the following programs to run 
+
+// program 1) :
+
+
+// let age = 18;
+
+// if (age >= 18){
+//     console.log("You can vote");
+// }
+// else{
+//     console.log("You can not vote");
+// }
+
+
+
+// program 2 :)
+
+
+// let marks = 85;
+
+// if(marks >= 90){
+//     console.log("Grade : A");
+// }
+// else if(marks >= 80){
+//     console.log("Grade : B");
+// }
+// else if(marks >= 70){
+//     console.log("Grade : C");
+// }
+// else if(markse >= 60){
+//     console.log("Grade : D");
+// }
+// else{
+//     console.log("Grade : F");
+// }
+
+
+
+// program 3 :)
+
+
+// let day = 3;
+// let dayName;
+
+// switch(day){
+//     case 1:
+//         dayName = "Monday";
+//         break;
+//     case 2:
+//         dayName = "Tuesday";
+//         break;
+//     case 3:
+//         dayName = "Wednesday";
+//         break;
+//     case 4:
+//         dayName = "Thursday";
+//         break;
+//     case 5:
+//         dayName = "Friday";
+//         break;
+//     case 6:
+//         dayName = "Saturday";
+//         break;
+//     case 7:
+//         dayName = "Sunday";
+//         break;
+//     default:
+//         dayName = "Invalid day";
+// }
+// console.log(dayName);
+
+
+// program 4 :)
+
+// let numbers = [3, 8, 1, 5, 9, 2];
+// let largest = numbers[0];
+
+// for (let i = 1; i < numbers.length; i++){
+//   if (numbers[i] > largest){
+//     largest = numbers[i];
+//   }
+// }
+// console.log("Largest number is:", largest);
+
+
+// program 5 :)
+
+
+// let numbers = [10, 20, 30, 40, 50];
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+// console.log("Sum of all elements is:", sum);
+
+
+// Program 6 :)
+
+// let num1 = parseFloat(prompt("Enter the first number:"));
+// let num2 = parseFloat(prompt("Enter the second number:"));
+
+// if (!isNaN(num1) && !isNaN(num2)) {
+//   let addition = num1 + num2;
+//   let subtraction = num1 - num2;
+//   let multiplication = num1 * num2;
+//   let division = num1 / num2;
+//   let modulus = num1 % num2;
+
+//   console.log("Addition:", addition);
+//   console.log("Subtraction:", subtraction);
+//   console.log("Multiplication:", multiplication);
+//   console.log("Division:", division);
+//   console.log("Modulus:", modulus);
+// } else {
+//   console.log("Please enter valid numbers.");
+// }
